fd8b2addddbc8306c8a7ec3e1af5228b100b8b8f
afaf driouech <afaf.driouech@gmail.com>
2021-04-03 12:18:33 +0100

    connection to firebase

--- app/src/main/java/com/example/donationapp/Registration.java
+++ app/src/main/java/com/example/donationapp/Registration.java
@@ -3,8 +3,21 @@ package com.example.donationapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
-public class Registration extends AppCompatActivity {
+public class Registration<FirebaseAuth> extends AppCompatActivity {
+
+    EditText FullName,Email,Password,City,Phone;
+    Button RegisterBtn;
+    TextView LoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+   // FirebaseFirestore fStore;
+    String userID;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
