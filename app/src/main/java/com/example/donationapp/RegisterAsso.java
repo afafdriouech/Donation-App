470c852ed371615c1a3183dee89086e4c41be3af
afaf driouech <afaf.driouech@gmail.com>
2021-04-03 17:41:45 +0100

    registration

---
+++ app/src/main/java/com/example/donationapp/RegisterAsso.java
@@ -0,0 +1,97 @@
+package com.example.donationapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterAsso extends AppCompatActivity {
+
+    EditText mFullName,mEmail,mPassword,mCity,mPhone;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth mAuth;
+    private ProgressBar progressBar;
+    //FirebaseFirestore fStore;
+    //String userID;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_asso);
+        mAuth = FirebaseAuth.getInstance();
+
+        mFullName   = findViewById(R.id.editTxtName);
+        mEmail      = findViewById(R.id.editTxtEmail);
+        mPassword   = findViewById(R.id.editTxtPassword);
+        mCity       =findViewById(R.id.editTxtCity);
+        mPhone      = findViewById(R.id.PhoneNumber);
+        mRegisterBtn= findViewById(R.id.btnRegister);
+        mLoginBtn   = findViewById(R.id.signin);
+        progressBar = findViewById(R.id.progressBar);
+
+        //fStore = FirebaseFirestore.getInstance();
+
+        if(mAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+                final String fullName = mFullName.getText().toString();
+                final String city = mCity.getText().toString();
+                final String phone = mPhone.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    mEmail.setError("Email is Required.");
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    mPassword.setError("Password is Required.");
+                    return;
+                }
+
+                if (password.length() < 6) {
+                    mPassword.setError("Password Must be >= 6 Characters");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //registration in firebase
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterAsso.this, "user created",Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }
+                        else{
+                            Toast.makeText(RegisterAsso.this, "Error "+task.getException().getMessage(),Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    });
+            }
+        }
+        );
+
+    }
+}
