d9267a8b8aed8dba623d6b12d6be53d6533f2579
bouchra08 <bouchra.mouhim99@gmail.com>
2021-04-23 21:30:29 +0100

    view project

--- app/src/main/java/com/example/donationapp/Project_details.java
+++ app/src/main/java/com/example/donationapp/Project_details.java
@@ -14,41 +14,12 @@ import android.widget.TextView;
 
 
 public class Project_details extends AppCompatActivity {
-    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_project_details);
-        drawerLayout = findViewById(R.id.drawer_layout);
-    }
-    public void ClickMenu(View view){
-        MenuNavigationActivity.openDrawer(drawerLayout);
-    }
 
-    public void ClickHome(View view){
-        MenuNavigationActivity.redirectActivity(this,Associations.class);
-    }
-    public void ClickProjet(View view){
-        MenuNavigationActivity.redirectActivity(this,liste_projets.class);
-    }
-
-    public void ClickDonationCall(View view){
-        MenuNavigationActivity.redirectActivity(this,Liste_appeldon.class);
-    }
-    public void ClickDonCalled(View view){
-        MenuNavigationActivity.redirectActivity(this,TestMenuActivity.class);
-    }
-    public void ClickDonators(View view){
-        MenuNavigationActivity.redirectActivity(this,Liste_donateurs.class);
-    }
-    public void ClickLogout(View view){
-        MenuNavigationActivity.logout(this);
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        MenuNavigationActivity.closeDrawer(drawerLayout);
     }
 
 }
